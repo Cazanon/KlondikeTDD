f2cd39b7655429a3740f5a4a55daf624ab006c69
Cazanon <carlos.azanon@gmail.com>
2015-04-15 12:18:48 +0200

    Implementado sizeTableausTest Modificado codigo para correcto funcionamiento de test

--- src/controllers/StartGameController.java
+++ src/controllers/StartGameController.java
@@ -1,5 +1,11 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+import models.Card;
+
+
 public class StartGameController {
 
 	public int sizeWaste() {
@@ -12,6 +18,25 @@ public class StartGameController {
 
 	public int sizeFoundations() {
 		return 0;
+	}
+
+	public ArrayList<Integer> sizeCoveredCardsTableaus() {
+		ArrayList<Integer> sizeCoveredCardsTableaus = new ArrayList<Integer>();
+		for(int i=0;i<7;i++){
+			sizeCoveredCardsTableaus.add(i);
+		}
+		return sizeCoveredCardsTableaus;
+	}
+
+	public ArrayList<Stack<Card>> uncoveredCardsStackTableaus() {
+		ArrayList<Stack<Card>> uncoveredCardsStackTableaus = new ArrayList<Stack<Card>>();
+		for(int i=0;i<7;i++){
+			Card card = new Card();
+			Stack<Card> stackCard = new Stack<Card>();
+			stackCard.add(card);
+			uncoveredCardsStackTableaus.add(stackCard);
+		}
+		return uncoveredCardsStackTableaus;
 	}
 
 }
--- src/tests/StartGameControllerTest.java
+++ src/tests/StartGameControllerTest.java
@@ -1,6 +1,11 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import models.Card;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,4 +36,20 @@ public class StartGameControllerTest {
 		assertEquals(0, startGameController.sizeFoundations());		
 	}
 	
+	@Test
+	public void sizeTableausTest() {
+		ArrayList<Integer> sizeTableaus = startGameController.sizeCoveredCardsTableaus();
+		ArrayList<Stack<Card>> uncoveredCardsStackTableaus = startGameController.uncoveredCardsStackTableaus();
+		
+		assertEquals(7, sizeTableaus.size());
+		assertEquals(7, uncoveredCardsStackTableaus.size());
+		
+		for (int i = 0; i < sizeTableaus.size(); i++) {
+			assertEquals(new Integer(i), sizeTableaus.get(i));
+		}
+		for (Stack<Card> uncoveredCardsStack : uncoveredCardsStackTableaus) {
+			assertEquals(1, uncoveredCardsStack.size());
+			assertTrue(uncoveredCardsStack.peek().uncovered()); //cima
+		}
+	}
 }
