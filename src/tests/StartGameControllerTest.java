1502d5fd66ab0788ee9f4ceda87b13cef378a40e
Cazanon <carlos.azanon@gmail.com>
2015-04-15 10:38:45 +0200

    Commit Inicial a partir de lo visto en clase

---
+++ src/tests/StartGameControllerTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Before;
+
+import controllers.StartGameController;
+
+public class StartGameControllerTest {
+
+	private StartGameController startGameController;
+	
+	@Before
+	public void before() {
+		startGameController = new StartGameController();
+	}
+	
+}
