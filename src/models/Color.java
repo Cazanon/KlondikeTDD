bfc5a4406ff67b487e05feb86ede105724f30f9b
Cazanon <carlos.azanon@gmail.com>
2015-04-16 12:27:46 +0200

    Creada clase Color con la asociacion de los palos al color

---
+++ src/models/Color.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Color {
+
+	public static final List<CardSuite> BLACK = Arrays.asList(CardSuite.CLUB,CardSuite.SPADE);
+	public static final List<CardSuite> RED = Arrays.asList(CardSuite.HEART,CardSuite.DIAMOND);
+	
+}
