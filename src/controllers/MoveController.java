a790e843a31dd1ce7ab8024fed82f8ad2f631cbd
Cazanon <carlos.azanon@gmail.com>
2015-04-17 12:46:47 +0200

    Refactorizado MoveController

--- src/controllers/MoveController.java
+++ src/controllers/MoveController.java
@@ -1,13 +1,15 @@
 package controllers;
 
-import java.util.Stack;
-
-import models.Card;
+import models.CardStack;
 
 public class MoveController {
 
-	public void move(Stack<Card> from, Stack<Card> to) {
-		to.push(from.pop());		
+	public boolean move(CardStack from, CardStack to) {
+		if(to.canPush(from.peek())){
+			to.push(from.pop());
+			return true;
+		}
+		return false;
 	}
 
 }
