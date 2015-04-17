3ebcffdd01afd03a7fdb1d888073299f72692954
Cazanon <carlos.azanon@gmail.com>
2015-04-17 12:55:31 +0200

    Implementado metodo move Deck to Waste en moveController (para dar la vuelta a la carta del mazo Deck

--- src/controllers/MoveController.java
+++ src/controllers/MoveController.java
@@ -1,10 +1,21 @@
 package controllers;
 
 import models.CardStack;
+import models.DeckStack;
+import models.WasteStack;
 
 public class MoveController {
 
 	public boolean move(CardStack from, CardStack to) {
+		if(to.canPush(from.peek())){
+			to.push(from.pop());
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean move(DeckStack from, WasteStack to) {
+		from.peek().setCovered(false);
 		if(to.canPush(from.peek())){
 			to.push(from.pop());
 			return true;
